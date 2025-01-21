1f054676e27a77607d0ad6d33e194293e1320549
Raiyan109 <kraiyan109@gmail.com>
2025-01-21 12:33:14 +0600

    auth pages done

--- src/pages/Auth/ResetPassword.jsx
+++ src/pages/Auth/ResetPassword.jsx
@@ -58,78 +58,78 @@ const ResetPassword = () => {
     // }
   };
   return (
-      <div className="min-h-[92vh] w-full grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-1 lg:gap-8">
-        <div className="lg:border-r-2 border-primary mx-auto w-[96%] lg:p-[10%] ">
-          <img src={image} alt="" />
-        </div>
-        <div className="lg:p-[5%] order-first lg:order-last">
-          <div className="w-full py-[44px] lg:px-[44px] space-y-8">
-            <div className="flex flex-col items-center lg:items-start">
-              <PageHeading backPath={-1} title={"Set new password"} disbaledBackBtn={true} />
-              <p className=" drop-shadow text-[#464343] mt-5">
-                Your password must be 8-10 character long.
-              </p>
-            </div>
-            <Form
-              name="normal_login"
-              layout="vertical"
-              initialValues={{
-                remember: true,
-              }}
-              requiredMark={false}
-              onFinish={onFinish}
-            >
-              <Form.Item
-                label={<span className="font-medium text-base">New Password</span>}
-                name="newPassword"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input new password!",
-                  },
-                ]}
-              >
-                <Input.Password size="large" placeholder="**********" />
-              </Form.Item>
-              <Form.Item
-                label={<span className="font-medium text-base">Confirm New Password</span>}
-                name="rePassword"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please Re-Enter the password!",
-                  },
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || getFieldValue("newPassword") === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        new Error(
-                          "The new password that you entered do not match!"
-                        )
-                      );
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password size="large" placeholder="**********" />
-              </Form.Item>
-              <div className="w-full flex justify-center pt-4 ">
-                  <Button
-                    // disabled={isLoading}
-                    type="primary"
-                    size="large"
-                    htmlType="submit"
-                    className="w-full px-2 "
-                  >
-                    Reset Password
-                  </Button>
-              </div>
-            </Form>
+    <div className="min-h-[92vh] w-full grid grid-cols-1 lg:grid-cols-2 justify-center items-center gap-1 lg:gap-8">
+      <div className="lg:border-r-2 border-primary mx-auto w-[96%] lg:p-[10%] ">
+        <img src={image} alt="" />
+      </div>
+      <div className="lg:p-[5%] order-first lg:order-first">
+        <div className="w-full py-[44px] lg:px-[44px] space-y-8">
+          <div className="flex flex-col items-center lg:items-start">
+            <PageHeading backPath={-1} title={"Reset password"} disbaledBackBtn={true} />
+            <p className=" drop-shadow text-[#464343] mt-5">
+              Your password must be 8-10 character long.
+            </p>
           </div>
+          <Form
+            name="normal_login"
+            layout="vertical"
+            initialValues={{
+              remember: true,
+            }}
+            requiredMark={false}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              // label={<span className="font-medium text-base">New Password</span>}
+              name="newPassword"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input new password!",
+                },
+              ]}
+            >
+              <Input.Password size="large" placeholder="Set your password" style={{ border: '1px solid #2781B5', borderRadius: '7px' }} />
+            </Form.Item>
+            <Form.Item
+              // label={<span className="font-medium text-base">Confirm New Password</span>}
+              name="rePassword"
+              rules={[
+                {
+                  required: true,
+                  message: "Please Re-Enter the password!",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("newPassword") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        "The new password that you entered do not match!"
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password size="large" placeholder="Re-enter password" style={{ border: '1px solid #2781B5', borderRadius: '7px' }} />
+            </Form.Item>
+            <div className="w-full flex justify-center pt-4 ">
+              <Button
+                // disabled={isLoading}
+                type="primary"
+                size="large"
+                htmlType="submit"
+                className="w-full px-2 bg-[#345C8C]"
+              >
+                Reset Password
+              </Button>
+            </div>
+          </Form>
         </div>
       </div>
+    </div>
   );
 };
 
